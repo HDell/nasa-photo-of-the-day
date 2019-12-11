8d50c858d4ccdbc43a8136f95417fed9a0240292
Handell Desulme <handelldesulme@Handells-MacBook-Pro.local>
2019-12-11 17:30:10 -0500

    completes stretch project

--- src/components/NasaList.js
+++ src/components/NasaList.js
@@ -4,9 +4,10 @@ import axios from "axios";
 
 const NasaList = () => {
     const [nasaData, setNasaData] = useState([]);
+    const [dateString, setDateString] = useState("2019-12-11");
 
     useEffect(() => {
-        axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${dateString}`)
             .then((response) => {
                 console.log(response);
                 setNasaData(response.data);
@@ -14,12 +15,20 @@ const NasaList = () => {
             .catch((error) => {
                 console.log("Failed to retrieve photo.")
             })
-    }, []);
+    }, [dateString]);
 
 
     return (
         <div>
             <NasaCard title={nasaData.title} date={nasaData.date} explanation={nasaData.explanation} hdurl={nasaData.hdurl}/>
+            <p>Test</p>
+            <div>
+                <select id="dropDown" onChange={() => setDateString(document.getElementById("dropDown").options[document.getElementById("dropDown").selectedIndex].value)}>
+                    <option value="2012-03-14">2012-03-14</option>
+                    <option value="2016-01-27">2016-01-27</option>
+                    <option selected value="2019-12-11">2019-12-11</option>
+                </select>
+            </div>
         </div>
     )
 }
