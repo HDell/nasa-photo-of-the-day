215bb116a1894d5f3f36497d0844427751cb13e0
Handell Desulme <handelldesulme@Handells-MacBook-Pro.local>
2019-12-12 18:50:41 -0500

    completes MVP with new date adding functionality

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,14 +1,19 @@
 import React from "react";
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Col} from 'reactstrap';
 
 const NasaCard = (props) => {
 
     return (
-        <div>
-            <h1>Title: {props.title}</h1>
-            <h2>Date: {props.date}</h2>
-            <p>Description: {props.explanation}</p>
-            <img src={props.hdurl}/>
-        </div>
+        <Col sm={{ size: 6, order: 2, offset: 1 }}>
+            <Card>
+                <CardImg src={props.hdurl}/>
+                <CardBody>
+                    <CardTitle>Title: {props.title}</CardTitle>
+                    <CardSubtitle>Date: {props.date}</CardSubtitle>
+                    <CardText>Description: {props.explanation}</CardText>
+                </CardBody>
+            </Card>
+        </Col>
     )
 }
 
--- src/components/NasaList.js
+++ src/components/NasaList.js
@@ -1,11 +1,26 @@
 import React, {useState, useEffect} from "react";
+import { Container, Row, Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import NasaCard from "./NasaCard";
 import axios from "axios";
 
 const NasaList = () => {
-    const [nasaData, setNasaData] = useState([]);
-    const [dateString, setDateString] = useState("2019-12-11");
+    //Date Setter
+    let dateObj = new Date();
+    let year = dateObj.getUTCFullYear();
+    let month = dateObj.getUTCMonth() + 1; //months from 1-12
+    let day = dateObj.getUTCDate();
+    let formattedDate = year + "-" + month + "-" + day;
 
+    //State
+    const [nasaData, setNasaData] = useState([]);
+    const [datesList, setDatesList] = useState([formattedDate]);
+    const [dateString, setDateString] = useState(datesList[0]);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    //Date Toggle
+    const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+    //Side Effects
     useEffect(() => {
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${dateString}`)
             .then((response) => {
@@ -19,16 +34,109 @@ const NasaList = () => {
 
 
     return (
-        <div>
-            <NasaCard title={nasaData.title} date={nasaData.date} explanation={nasaData.explanation} hdurl={nasaData.hdurl}/>
-            <div>
-                <select id="dropDown" onChange={() => setDateString(document.getElementById("dropDown").options[document.getElementById("dropDown").selectedIndex].value)}>
-                    <option value="2012-03-14">2012-03-14</option>
-                    <option value="2016-01-27">2016-01-27</option>
-                    <option selected value="2019-12-11">2019-12-11</option>
-                </select>
-            </div>
-        </div>
+        <Container>
+            <Row>
+                <NasaCard title={nasaData.title} date={nasaData.date} explanation={nasaData.explanation} hdurl={nasaData.hdurl}/>
+                <Col>
+                    <Form>
+                        <FormGroup>
+                            <Label for="yearSelect">Year</Label>
+                            <Input type="select" name="select" id="yearSelect">
+                                <option>2000</option>
+                                <option>2001</option>
+                                <option>2002</option>
+                                <option>2003</option>
+                                <option>2004</option>
+                                <option>2005</option>
+                                <option>2006</option>
+                                <option>2007</option>
+                                <option>2008</option>
+                                <option>2009</option>
+                                <option>2010</option>
+                                <option>2011</option>
+                                <option>2012</option>
+                                <option>2013</option>
+                                <option>2014</option>
+                                <option>2015</option>
+                                <option>2016</option>
+                                <option>2017</option>
+                                <option>2018</option>
+                                <option>2019</option>
+                            </Input>
+                            <Label for="monthSelect">Month</Label>
+                            <Input type="select" name="select" id="monthSelect">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                                <option>7</option>
+                                <option>8</option>
+                                <option>9</option>
+                                <option>10</option>
+                                <option>11</option>
+                                <option>12</option>
+                            </Input>
+                            <Label for="daySelect">Date</Label>
+                            <Input type="select" name="select" id="daySelect">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                                <option>7</option>
+                                <option>8</option>
+                                <option>9</option>
+                                <option>10</option>
+                                <option>11</option>
+                                <option>12</option>
+                                <option>13</option>
+                                <option>14</option>
+                                <option>15</option>
+                                <option>16</option>
+                                <option>17</option>
+                                <option>18</option>
+                                <option>19</option>
+                                <option>20</option>
+                                <option>21</option>
+                                <option>22</option>
+                                <option>23</option>
+                                <option>24</option>
+                                <option>25</option>
+                                <option>26</option>
+                                <option>27</option>
+                                <option>28</option>
+                                <option>29</option>
+                                <option>30</option>
+                            </Input>
+                        </FormGroup>
+                        <Button onClick={() => {
+                            let yearElem = document.getElementById("yearSelect");
+                            let monthElem = document.getElementById("monthSelect");
+                            let dayElem = document.getElementById("daySelect");
+                            year = yearElem.options[yearElem.selectedIndex].text;
+                            month = monthElem.options[monthElem.selectedIndex].text;
+                            day = dayElem.options[dayElem.selectedIndex].text;
+                            formattedDate = year + "-" + month + "-" + day;
+                            datesList.push(formattedDate);
+                        }}>Add Date</Button>
+                    </Form>
+                    <br/>
+                    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                        <DropdownToggle caret>
+                            Dates
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            {datesList.map((dateOption, index) => (
+                                <DropdownItem onClick={() => setDateString(datesList[index])}>{dateOption}</DropdownItem>
+                            ))}
+                        </DropdownMenu>
+                    </Dropdown>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
